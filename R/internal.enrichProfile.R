1c784cf169bda2ac5ddc8651cca0d6850a051c9d
Alex Sánchez <asanchez@ub.edu>
2010-04-19 20:11:06 +0000

    update class by class comparison functions using Fisher test
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/goProfiles@46232 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/internal.enrichProfile.R
@@ -0,0 +1,11 @@
+
+internal.enrichProfile <- function(prof, qrof, n, m, method, ...) {
+  s <- length(prof)
+  pvals <- numeric(s)
+  for (i in 1:s){
+    freqs_i <- trunc(matrix(c(prof[i], n - prof[i], qrof[i], m - qrof[i]), nrow=2))
+    pvals[i] <- fisher.test(freqs_i)$p.value
+  }
+  names(pvals) <- names(prof)
+  p.adjust(pvals, method=method, ...)
+}
