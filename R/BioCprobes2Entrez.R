2f5703fe4674639ac57e583039464887de0f3583
Alex Sánchez <asanchez@ub.edu>
2016-02-05 14:58:57 +0000

    Fixed compilation errors by importing AnnotationDbi, Biobase, GO.db. Update Bioconductor Views File
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/goProfiles@113318 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/BioCprobes2Entrez.R
+++ R/BioCprobes2Entrez.R
@@ -3,7 +3,7 @@ function (probeslist , anotPkg, na.rm=TRUE)
 {
 stopifnot(require(anotPkg, character.only = TRUE, quietly=TRUE, warn.conflicts=FALSE))
 myenvir<-eval(parse(text = paste(anotPkg,"ENTREZID",sep="")))
-myENTREZIDs <- as.character(unlist(mget(probeslist,env=myenvir, ifnotfound=NA)))
+myENTREZIDs <- as.character(unlist(mget(probeslist,envir=myenvir, ifnotfound=NA)))
 if(na.rm){
   return(myENTREZIDs[!(is.na(myENTREZIDs))])
 }else{
